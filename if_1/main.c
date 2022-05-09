eb478ef739793983077bb18cafe5caa071324d5f
jeonghokyoung <mikapuking@gmail.com>
2022-05-09 17:09:46 +0900

    초기 커밋.

---
+++ if_1/main.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include	<stdio.h>
+
+void main() {
+	int i = 3;
+
+	// if! 개별검사해야할때는 if > if > if
+	// (처음 if가 만족해도 다음 if문을 검사)
+	// 
+	//  하나의 조건에만 만족할때는 if > else if > else if 
+	//  (하나 만족하면 아래 문장은 실행 안함 )
+
+	/*if (i == 1) {
+		printf("1을 출력합니다.\n");
+	}
+	else if (i == 2) {
+		printf("2을 출력합니다.\n");
+	}
+	else if (i == 3) {
+		printf("3을 출력합니다.\n");
+	}
+	else{
+		printf("1, 2, 3이 아닌 수를 출력합니다.\n");
+	}*/
+}
